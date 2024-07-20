353710a0044ba78471f5f0d80880ca73fbc7fd11
caspero94 <84985267+caspero94@users.noreply.github.com>
2024-07-20 17:28:13 +0000

    ds

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,6 +53,8 @@ def plot_chart(exchange ,symbol, timeframe):
         chart.load()
 
 # Interfaz de usuario
+selected_exchange, selected_symbol, selected_timeframe = st.columns(3, vertical_alignment="bottom")
+
 exchange = ["Binance"]
 symbols = ["BTCUSDT", "ETHUSDT"]  # Ajusta según tus tickers
 timeframes = ["1m", "3m", "5m", "15m", "30m", "1h", "2h", "4h", "6h", "8h", "12h", "1d"]
