688923534a11eed1a3a18356bcd271614bf09607
caspero94 <84985267+caspero94@users.noreply.github.com>
2024-07-20 15:09:05 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,11 +18,11 @@ def transform_data(data):
     transformed = []
     for entry in data:
         transformed_entry = {
-            "open": float(entry["open"]),
+            "open": int(float(entry["open"])),  # Convertir a int
             "high": float(entry["high"]),
             "low": float(entry["low"]),
             "close": float(entry["close"]),
-            "time": int(entry["open_time"] / 1000)  # Convert milliseconds to seconds
+            "time": int(entry["open_time"] / 1000)
         }
         transformed.append(transformed_entry)
     return transformed
@@ -32,25 +32,12 @@ url = 'http://104.46.208.49:8000/api/klines/binance?ticker=ETHUSDT&timeframe=1m&
 data = asyncio.run(fetch_data(url))
 transformed_data = transform_data(data)
 
-st.write("COMPRO TRANSFORMED")
-for entry in transformed_data:
-    st.write(f"Types: open={type(entry['open'])}, high={type(entry['high'])}, low={type(entry['low'])}, close={type(entry['close'])}, time={type(entry['time'])}")
+st.write("Datos Originales:")
+st.write(data)
 
-datase = [
-        { "open": 10, "high": 10.63, "low": 9.49, "close": 9.55, "time": 1642427876 },
-        { "open": 9.55, "high": 10.30, "low": 9.42, "close": 9.94, "time": 1642514276 },
-        { "open": 9.94, "high": 10.17, "low": 9.92, "close": 9.78, "time": 1642600676 },
-        { "open": 9.78, "high": 10.59, "low": 9.18, "close": 9.51, "time": 1642687076 },
-        { "open": 9.51, "high": 10.46, "low": 9.10, "close": 10.17, "time": 1642773476 },
-        { "open": 10.17, "high": 10.96, "low": 10.16, "close": 10.47, "time": 1642859876 },
-        { "open": 10.47, "high": 11.39, "low": 10.40, "close": 10.81, "time": 1642946276 },
-        { "open": 10.81, "high": 11.60, "low": 10.30, "close": 10.75, "time": 1643032676 },
-        { "open": 10.75, "high": 11.60, "low": 10.49, "close": 10.93, "time": 1643119076 },
-        { "open": 10.93, "high": 11.53, "low": 10.76, "close": 10.96, "time": 1643205476 }
-    ]
-st.write("COMPROBADNO DATASE")
-for entry in datase:
-    st.write(f"Types: open={type(entry['open'])}, high={type(entry['high'])}, low={type(entry['low'])}, close={type(entry['close'])}, time={type(entry['time'])}")
+st.write("Datos Transformados:")
+st.write(transformed_data)
+
 # Configuración del gráfico
 chartOptions = {
     "layout": {
@@ -62,10 +49,9 @@ chartOptions = {
     }
 }
 
-# Usar datos estáticos para verificar el gráfico
 seriesCandlestickChart = [{
     "type": 'Candlestick',
-    "data": datase,  # Intenta primero con datos transformados completos
+    "data": transformed_data,
     "options": {
         "upColor": COLOR_BULL,
         "downColor": COLOR_BEAR,
