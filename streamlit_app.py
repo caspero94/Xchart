7182f2def68fc068847ad6964f55da43d57ffc65
caspero94 <84985267+caspero94@users.noreply.github.com>
2024-07-20 17:54:42 +0000

    0.10

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ def plot_chart(exchange ,symbol, timeframe):
         # Mostrar gráfico
     chart = StreamlitChart(height=1000)
     chart.legend(visible=True)
-    chart.volume_config(scale_margin_top=0.90)
+    chart.volume_config(scale_margin_top=0.10)
     chart.set(df)
     chart.load()
 
