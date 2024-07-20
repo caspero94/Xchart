33a195b2b8d6a1b866e63e3b55edc75102e7da2e
caspero94 <84985267+caspero94@users.noreply.github.com>
2024-07-20 14:55:04 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit as st
-import json
-import aiohttp
 import asyncio
+import aiohttp
 from streamlit_lightweight_charts import renderLightweightCharts
 
+# Colores para las velas
 COLOR_BULL = '#26a69a'
 COLOR_BEAR = '#ef5350'
 
@@ -23,7 +23,7 @@ def transform_data(data):
             "high": float(entry["high"]),
             "low": float(entry["low"]),
             "close": float(entry["close"]),
-            "time": int(entry["open_time"] / 1000)
+            "time": int(entry["open_time"] / 1000)  # Convert milliseconds to seconds
         }
         transformed.append(transformed_entry)
     return transformed
@@ -33,12 +33,13 @@ url = 'http://104.46.208.49:8000/api/klines/binance?ticker=ETHUSDT&timeframe=1m&
 data = asyncio.run(fetch_data(url))
 transformed_data = transform_data(data)
 
-st.write("Original Data:")
+st.write("Datos Originales:")
 st.write(data)
 
-st.write("Transformed Data:")
+st.write("Datos Transformados:")
 st.write(transformed_data)
 
+# Configuración del gráfico
 chartOptions = {
     "layout": {
         "textColor": 'black',
@@ -49,9 +50,24 @@ chartOptions = {
     }
 }
 
+# Datos de prueba estáticos
+datase = [
+    { "open": 10, "high": 10.63, "low": 9.49, "close": 9.55, "time": 1642427876 },
+    { "open": 9.55, "high": 10.30, "low": 9.42, "close": 9.94, "time": 1642514276 },
+    { "open": 9.94, "high": 10.17, "low": 9.92, "close": 9.78, "time": 1642600676 },
+    { "open": 9.78, "high": 10.59, "low": 9.18, "close": 9.51, "time": 1642687076 },
+    { "open": 9.51, "high": 10.46, "low": 9.10, "close": 10.17, "time": 1642773476 },
+    { "open": 10.17, "high": 10.96, "low": 10.16, "close": 10.47, "time": 1642859876 },
+    { "open": 10.47, "high": 11.39, "low": 10.40, "close": 10.81, "time": 1642946276 },
+    { "open": 10.81, "high": 11.60, "low": 10.30, "close": 10.75, "time": 1643032676 },
+    { "open": 10.75, "high": 11.60, "low": 10.49, "close": 10.93, "time": 1643119076 },
+    { "open": 10.93, "high": 11.53, "low": 10.76, "close": 10.96, "time": 1643205476 }
+]
+
+# Usar los datos estáticos para verificar el gráfico
 seriesCandlestickChart = [{
     "type": 'Candlestick',
-    "data": transformed_data,
+    "data": datase,
     "options": {
         "upColor": COLOR_BULL,
         "downColor": COLOR_BEAR,
@@ -61,7 +77,7 @@ seriesCandlestickChart = [{
     }
 }]
 
-st.subheader("Candlestick Chart with Watermark")
+st.subheader("Candlestick Chart with Test Data")
 
 renderLightweightCharts([
     {
