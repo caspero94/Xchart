7a400d58957d0b92ba06746543f806dc45d193cd
caspero94 <84985267+caspero94@users.noreply.github.com>
2024-07-20 14:12:43 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,44 @@
 import streamlit as st
+import json
+import requests
 from streamlit_lightweight_charts import renderLightweightCharts
+from abc import ABC, abstractmethod
+import datetime
+
+
+def fetch_data(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        response.raise_for_status()
+
+def transform_data(data):
+    transformed = []
+    for entry in data:
+        transformed_entry = {
+            "open": float(entry["open"]),
+            "high": float(entry["high"]),
+            "low": float(entry["low"]),
+            "close": float(entry["close"]),
+            "time": int(entry["open_time"] / 1000)  # Convert milliseconds to seconds
+        }
+        transformed.append(transformed_entry)
+    return transformed
+
+def save_json(data, filename):
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=4)
+
+# URL de la API
+url = 'http://104.46.208.49:8000/api/klines/binance?ticker=ETHUSDT&timeframe=1m&limit=5'
+
+# Obtener los datos de la API
+data = fetch_data(url)
+
+# Transformar los datos
+transformed_data = transform_data(data)
+
 
 chartOptions = {
     "layout": {
@@ -13,18 +52,7 @@ chartOptions = {
 
 seriesCandlestickChart = [{
     "type": 'Candlestick',
-    "data": [
-        { "open": 10, "high": 10.63, "low": 9.49, "close": 9.55, "time": 1642427876 },
-        { "open": 9.55, "high": 10.30, "low": 9.42, "close": 9.94, "time": 1642514276 },
-        { "open": 9.94, "high": 10.17, "low": 9.92, "close": 9.78, "time": 1642600676 },
-        { "open": 9.78, "high": 10.59, "low": 9.18, "close": 9.51, "time": 1642687076 },
-        { "open": 9.51, "high": 10.46, "low": 9.10, "close": 10.17, "time": 1642773476 },
-        { "open": 10.17, "high": 10.96, "low": 10.16, "close": 10.47, "time": 1642859876 },
-        { "open": 10.47, "high": 11.39, "low": 10.40, "close": 10.81, "time": 1642946276 },
-        { "open": 10.81, "high": 11.60, "low": 10.30, "close": 10.75, "time": 1643032676 },
-        { "open": 10.75, "high": 11.60, "low": 10.49, "close": 10.93, "time": 1643119076 },
-        { "open": 10.93, "high": 11.53, "low": 10.76, "close": 10.96, "time": 1643205476 }
-    ],
+    "data": transformed_data,
     "options": {
         "upColor": '#26a69a',
         "downColor": '#ef5350',
