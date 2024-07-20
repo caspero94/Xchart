dbefe036edaf62ef5dad090c314f181e03e1f019
caspero94 <84985267+caspero94@users.noreply.github.com>
2024-07-20 16:04:22 +0000

    ss

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ def plot_chart(symbol, timeframe):
     
     df['open_time'] = pd.to_datetime(df['open_time'], unit='ms')
     df.rename(columns={'open_time': 'date'}, inplace=True)
-    df.drop(columns=['close_time', 'base_asset_volume','number_of_trades','taker_buy_volume'], inplace=True)
+    df.drop(columns=['close_time', 'base_asset_volume','number_of_trades','taker_buy_volume','taker_buy_base_asset_volume'], inplace=True)
     st.write(df)
 
     # Mostrar gráfico
