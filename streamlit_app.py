824f8e6e4647dd83add39872c47b9f39b34edac5
caspero94 <84985267+caspero94@users.noreply.github.com>
2024-07-21 10:22:46 +0000

    update timezone

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ def plot_chart(exchange, symbol, timeframe, timezone):
 
     # Convertir la fecha a la zona horaria seleccionada
     df.index = df.index.tz_localize('UTC').tz_convert(timezone)
-    st.write(df)
+    df.index = df.index.tz_localize(None)
     
     # Mostrar gráfico
     chart = StreamlitChart(height=1000)
